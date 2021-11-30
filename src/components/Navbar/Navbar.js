e47a6a9bc5fa523503b98628d42003bfea97cc16
Martin Heredia <Martin Heredia>
2021-11-30 17:19:46 -0300

    Correccion de estilos, integracion de itemDetail, y ItemDetailContainer, añadidas las rutas sobre el navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import CartWidget from "../Cart/CartWidget";
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
-import react from "react";
 
 const Navbar = () => {
-  const [clickeado, setClickeado] = react.useState(false);
+  const [clickeado, setClickeado] = useState(false);
 
-  const clickearMenu = () => {
-    setClickeado((current) => !current);
-  };
+  const toggle =() => setClickeado(!clickeado)
+  const esconder = () => setClickeado(false)
+  const mostrar = () => setClickeado(true)
 
   return (
     <nav className="NavbarItems">
       <h1 className="navbarLogo">
         <Link to="/" className="logolink" ><i className="fa-brands fa-usps"></i> <span className="nameShop">SKTSHOP</span></Link>
       </h1>
-      <div className="menuIcono" onClick={clickearMenu}>
+      <div className="menuIcono" onClick={toggle}>
         <i className={clickeado ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
       <ul className={clickeado ? "navMenu activo" : "navMenu"}>
-        <NavLink to="/" className="nav-links">
+        <NavLink to="/" onClick={toggle} onBlur={esconder} onFocus={mostrar}  className="nav-links">
           Home
         </NavLink>
-        <NavLink to="/category/Skate" className="nav-links">
+        <NavLink to="/category/Skate" onClick={toggle} onBlur={esconder} onFocus={mostrar} className="nav-links">
           Skates
         </NavLink>
-        <NavLink to="/category/Calzado" className="nav-links">
+        <NavLink to="/category/Calzado" onClick={toggle} onBlur={esconder} onFocus={mostrar}  className="nav-links">
           Calzado
         </NavLink>
       </ul>
