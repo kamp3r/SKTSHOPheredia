67b00d984fcd0e59fb86f286771b5c9981c65342
Martin Heredia <Martin Heredia>
2021-12-13 00:22:06 -0300

    env, firebase, y firestore aplicado

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPrYdJ6_yEDlXUe4MyzfI1MFWlaaUPtMA",
-  authDomain: "sktshop-db.firebaseapp.com",
-  projectId: "sktshop-db",
-  storageBucket: "sktshop-db.appspot.com",
-  messagingSenderId: "824844711241",
-  appId: "1:824844711241:web:50445bdbc14b81a36f5698"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
