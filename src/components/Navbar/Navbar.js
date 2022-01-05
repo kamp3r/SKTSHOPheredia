0198b2cbf5825afac24e88a7e97355fad80fd528
Martin Heredia <Martin Heredia>
2022-01-05 12:09:54 -0300

    Correccion de estilos para presentacion final

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,6 +11,8 @@ const Navbar = () => {
   const { usuarioGlobal } = useContext(UserContext);
 
   const toggle = () => setClickeado(!clickeado);
+  const ocultar = () => setClickeado(false)
+  const mostrar = () => setClickeado(true)
 
   return (
     <nav className="NavbarItems">
@@ -24,26 +26,27 @@ const Navbar = () => {
         <i className={clickeado ? "fas fa-times" : "fas fa-bars"}></i>
       </div>
       <ul className={clickeado ? "navMenu activo" : "navMenu"}>
-        <NavLink to="/" onClick={toggle} className="nav-links">
+        <NavLink to="/" onClick={toggle} onBlur={ocultar} onFocus={mostrar} className="nav-links">
           Home
         </NavLink>
-        <NavLink to="/category/Skates" onClick={toggle} className="nav-links">
+        <NavLink to="/category/Skates" onClick={toggle} onBlur={ocultar} onFocus={mostrar} className="nav-links">
           Skates
         </NavLink>
-        <NavLink to="/category/Rollers" onClick={toggle} className="nav-links">
+        <NavLink to="/category/Rollers" onClick={toggle} onBlur={ocultar} onFocus={mostrar} className="nav-links">
           Rollers
         </NavLink>
-        <NavLink to="/category/BMX" onClick={toggle} className="nav-links">
+        <NavLink to="/category/BMX" onClick={toggle} onBlur={ocultar} onFocus={mostrar} className="nav-links">
           BMX
         </NavLink>
-        <NavLink to="/MyAccount" onClick={toggle} className="nav-links">
+        <NavLink to="/MyAccount" onClick={toggle} onBlur={ocultar} onFocus={mostrar} className="nav-links">
           {usuarioGlobal ? "Mi cuenta" : "Iniciar Sesion"}
         </NavLink>
       </ul>
+      
       {cart.length ? (
-        <Link to="/Cart">
+        <NavLink onClick={ocultar} to="/Cart" >
           <CartWidget />
-        </Link>
+        </NavLink>
       ) : (
         <div></div>
       )}
